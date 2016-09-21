125986402cf08b1ca854d126919885db312417f9
gatblau <andres.alos@ntlworld.com>
2016-09-22 00:48:14 +0100

    Initial commit

--- images/lynx-data/start.sh
+++ images/lynx-data/start.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-function on_error() {
-  echo "Error on line $1"
-  exit 1
-}
-trap 'on_error $LINENO' ERR
 
-DATADIR="/var/lib/mysql"
+MySqlInitFile='/config/mysql-init.sql'
 
-if [ ! -d "$DATADIR/mysql" ]; then
+echo 'Checking the database server has been initialised'
+if [ ! -e "$MySqlInitFile" ]; then
+    echo 'The database server is not initialised.'
+    echo 'Attempting to initialise the server.'
+    echo 'Checking for required environment variables.'
     if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
         echo >&2 'error: MYSQL_ROOT_PASSWORD not set'
         echo >&2 '  Did you forget to add -e MYSQL_ROOT_PASSWORD=... ?'
@@ -18,21 +17,24 @@ if [ ! -d "$DATADIR/mysql" ]; then
         echo >&2 '  Did you forget to add -e MYSQL_PASSWORD=... ?'
         exit 1
     fi
-    mysql_install_db --datadir="$DATADIR"
 
-    initSqlFile='/config/mysql-init.sql'
-
-    cat >> "$initSqlFile" <<-EOSQL
+    echo 'Creating mysql-init file.'
+    echo 'Appending Users and Databases to mysql-init file'
+    cat > "$MySqlInitFile" <<-EOSQL
+FLUSH PRIVILEGES ;
 DELETE FROM mysql.user ;
 CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
 GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
 DROP DATABASE IF EXISTS test ;
-CREATE DATABASE IF NOT EXISTS 'lynxc' DEFAULT CHARACTER SET utf8 ;
+CREATE DATABASE IF NOT EXISTS lynxc DEFAULT CHARACTER SET utf8 ;
 CREATE USER 'lynx'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;
-GRANT ALL ON 'lynxc'.* TO 'lynx'@'%' ;
+GRANT ALL ON lynxc.* TO 'lynx'@'%' ;
+FLUSH PRIVILEGES ;
 EOSQL
+    echo 'Appending tables to mysql-init file'
+    cat '/config/tables.sql' >> "$MySqlInitFile"
 
-    cat '/config/tables.sql' >> "$initSqlFile"
 fi
 
+echo 'Executing mysqld_safe'
 exec mysqld_safe
