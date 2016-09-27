85a2b2a04192d6f51a7f702e36bfa74422d8b3ac
europa <andres.alos@ntlworld.com>
2016-09-27 22:30:00 +0100

    Initial commit

--- images/lynx-data/rebuild.sh
+++ images/lynx-data/rebuild.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+echo '--> Removing existing container...'
 docker rm -f lynxdb
+echo '--> Removing exisitng image...'
 docker rmi gatblau/lynx-data
+echo '--> Rebuilding image...'
 sh build.sh
+echo '--> Launching container...'
 sh run.sh
