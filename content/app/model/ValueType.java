908ba4482a20ecc3461e251455a17b549e27baab
europa <andres.alos@ntlworld.com>
2016-10-01 14:28:15 +0100

    Initial commit

--- content/app/model/ValueType.java
+++ content/app/model/ValueType.java
@@ -8,9 +8,9 @@ import java.util.Set;
 @Entity (name="ValueType")
 @Table (name="\"value_type\"")
 @NamedQueries ({
-	 @NamedQuery(name="ValueType.findAll", query="SELECT a FROM ValueType a")
-	,@NamedQuery(name="ValueType.findByName", query="SELECT a FROM ValueType a WHERE a.name = :name")
-	,@NamedQuery(name="ValueType.findByNameContaining", query="SELECT a FROM ValueType a WHERE a.name like :name")
+        @NamedQuery(name="ValueType.findAll", query="SELECT a FROM ValueType a")
+        ,@NamedQuery(name="ValueType.findByName", query="SELECT a FROM ValueType a WHERE a.name = :name")
+        ,@NamedQuery(name="ValueType.findByNameContaining", query="SELECT a FROM ValueType a WHERE a.name like :name")
 })
 public class ValueType implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -18,49 +18,49 @@ public class ValueType implements Serializable {
     public static final String FIND_ALL = "ValueType.findAll";
     public static final String FIND_BY_NAME = "ValueType.findByName";
     public static final String FIND_BY_NAME_CONTAINING ="ValueType.findByNameContaining";
-	
-    @Id @Column(name="id" ) 
+
+    @Id @Column(name="id" )
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name="name"  , length=45 , nullable=false , unique=false)
-    private String name; 
+    private String name;
 
     @OneToMany (targetEntity=model.ValueDef.class, fetch=FetchType.LAZY, mappedBy="valueTypeId", cascade=CascadeType.REMOVE)//, cascade=CascadeType.ALL)
-    private Set <ValueDef> valueDefValueTypeViaValueTypeId = new HashSet<ValueDef>(); 
+    private Set <ValueDef> valueDefValueTypeViaValueTypeId = new HashSet<ValueDef>();
 
     public ValueType() {
     }
 
     public ValueType(
-       Integer id,
-       String name) {
-	 this(
-       id,
-       name
-	 ,true);
-	}
-    
-	public ValueType(
-       Integer id,
-       String name	
-    , boolean setRelationship) {
-       setId (id);
-       setName (name);
+            Integer id,
+            String name) {
+        this(
+                id,
+                name
+                ,true);
     }
 
-	public ValueType flat() {
-	   return new ValueType(
-          getId(),
-          getName()
-       , false
-	   );
-	}
+    public ValueType(
+            Integer id,
+            String name
+            , boolean setRelationship) {
+        setId (id);
+        setName (name);
+    }
+
+    public ValueType flat() {
+        return new ValueType(
+                getId(),
+                getName()
+                , false
+        );
+    }
 
     public Integer getId() {
         return id;
     }
-	
+
     public void setId (Integer id) {
         this.id =  id;
     }
@@ -68,7 +68,7 @@ public class ValueType implements Serializable {
     public String getName() {
         return name;
     }
-	
+
     public void setName (String name) {
         this.name =  name;
     }
@@ -82,9 +82,9 @@ public class ValueType implements Serializable {
 
     public void setValueDefValueTypeViaValueTypeId (Set<ValueDef> valueDefValueTypeViaValueTypeId) {
         this.valueDefValueTypeViaValueTypeId = valueDefValueTypeViaValueTypeId;
-    }	
-    
+    }
+
     public void addValueDefValueTypeViaValueTypeId (ValueDef element) {
-    	    getValueDefValueTypeViaValueTypeId().add(element);
+        getValueDefValueTypeViaValueTypeId().add(element);
     }
 }
