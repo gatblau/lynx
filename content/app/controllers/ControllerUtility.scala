652a5a4b6a728e31ff979b762fc1070b8d59e818
europa <andres.alos@ntlworld.com>
2016-10-07 21:08:05 +0100

    Initial commit

--- content/app/controllers/ControllerUtility.scala
+++ content/app/controllers/ControllerUtility.scala
@@ -7,23 +7,12 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import lynx.api.ApiResult
 import play.api.http.Writeable
-import play.api.libs.json.{Json, Writes}
+import play.api.libs.json.Json
 import play.api.mvc.{Codec, Controller, Result}
 
 trait ControllerUtility extends Controller {
 
-//  implicit val resultWrites = new Writes[ApiResult] {
-//    def writes(result: ApiResult) = Json.obj(
-//      "id" -> result.id,
-//      "success" -> result.success,
-//      "message" -> result.message
-//    )
-//  }
-
   implicit val apiResultFormat = Json.format[ApiResult]
-
-  implicit def writeableOf_List(implicit codec: Codec): Writeable[List[ApiResult]] =
-    Writeable(list => ByteString(mapper.writeValueAsString(list)), Some(MediaType.APPLICATION_JSON))
 
   implicit def writeableOf_Array(implicit codec: Codec): Writeable[Array[ApiResult]] =
     Writeable(array => ByteString(mapper.writeValueAsString(array)), Some(MediaType.APPLICATION_JSON))
